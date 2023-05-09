9d3941444b7a68847bdc135d39aa0673b313a4f0
Amadu <amaduhamzajaarah300@gmail.com>
2023-05-09 00:09:54 +0000

    print int from 0 to 1o

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,6 +4,7 @@
  * main - program that prints all single digit numbers of base 10 starting from
  * All your code should be in the main function
  * Return: 0
+ * Author : Amadu Hamza
  */
 
 int main(void)
